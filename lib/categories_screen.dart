bb1b194dca021ed25e17f4b111819c304799ae58
Jash <jashshah3005@gmail.com>
2022-05-04 19:42:58 +0530

    Added fonts and edited styling

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('Meal Time')),
       body: GridView(
+        padding: EdgeInsets.all(25),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
